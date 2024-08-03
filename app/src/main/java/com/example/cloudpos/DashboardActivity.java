956296c8ad50d511d8516063be7f352ce4fc487f
BuildTools <unconfigured@null.spigotmc.org>
2024-08-03 19:36:36 +0700

    New Updates

--- app/src/main/java/com/example/cloudpos/DashboardActivity.java
+++ app/src/main/java/com/example/cloudpos/DashboardActivity.java
@@ -1,6 +1,9 @@
 package com.example.cloudpos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DashboardActivity extends AppCompatActivity {
@@ -9,5 +12,14 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+
+        ImageView userProfile = findViewById(R.id.userProfile);
+
+        userProfile.setOnClickListener(v -> {
+            // Create a PopupMenu
+            Intent intent = new Intent(DashboardActivity.this, UserProfile.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
---
+++ app/src/main/java/com/example/cloudpos/UserProfile.java
@@ -0,0 +1,20 @@
+package com.example.cloudpos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userprofile);
+    }
+}
